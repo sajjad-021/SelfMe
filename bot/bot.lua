1b1406b482cca0dbf1cf3b3e66acce7895331d88
Electrovirus <E.electrovirus@gmail.com>
2016-11-18 18:57:38 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -232,7 +232,7 @@ function create_config( )
 	"botmanager",
     "tabchi"
     },
-    sudo_users = {122774063},--Sudo users
+    sudo_users = {184018132},--Sudo users
   }
   serialize_to_file(config, './data/config.lua')
   print('saved config into ./data/config.lua')
